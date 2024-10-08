97521513c9f0f917bc5eb16d1874f22c529b7553
Diggo <rdiggosilva@gmail.com>
2024-10-08 12:01:32 -0300

    Starting buildind app

---
+++ PetBreed/App/Scenes/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  PetBreed
+//
+//  Created by Diggo Silva on 08/10/24.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
