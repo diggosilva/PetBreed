3585acf3241bb48495ab92a00e2883f033f3cfdf
Diggo <rdiggosilva@gmail.com>
2024-10-08 12:19:00 -0300

    Implemented tableView in FeedViewController

--- PetBreed/App/Scenes/Feed/FeedViewController.swift
+++ PetBreed/App/Scenes/Feed/FeedViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    let feedView = FeedView()
+    
+    override func loadView() {
+        super.loadView()
+        view = feedView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setNavBar()
+        setDelegatesAndDataSources()
+    }
+    
+    private func setNavBar() {
+        title = "Raça dos Pets"
+    }
+    
+    private func setDelegatesAndDataSources() {
+        feedView.tableView.delegate = self
+        feedView.tableView.dataSource = self
     }
 
+}
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text  = "Hello World"
+        return cell
+    }
 }
 
 class FeedView: UIView {
