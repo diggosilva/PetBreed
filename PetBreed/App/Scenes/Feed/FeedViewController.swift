73e25f279db2b5bea8989cd0901f6b37512a4375
Diggo <rdiggosilva@gmail.com>
2024-10-08 12:10:25 -0300

    Created tableView in FeedView

---
+++ PetBreed/App/Scenes/Feed/FeedViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FeedViewController.swift
+//  PetBreed
+//
+//  Created by Diggo Silva on 08/10/24.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+class FeedView: UIView {
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy () {
+        backgroundColor = .systemBackground
+        addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
