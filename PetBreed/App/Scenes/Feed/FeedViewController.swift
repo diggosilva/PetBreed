3c24b31d5b3cc544f5620fed0e96709dcef36a15
Diggo <rdiggosilva@gmail.com>
2024-10-09 18:08:21 -0300

    Created a custom cell

--- PetBreed/App/Scenes/Feed/FeedViewController.swift
+++ PetBreed/App/Scenes/Feed/FeedViewController.swift
@@ -39,8 +39,8 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text  = "Hello World"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedCell.identifier, for: indexPath) as? FeedCell else { return UITableViewCell() }
+//        cell.textLabel?.text  = "Hello World"
         return cell
     }
 }
@@ -49,7 +49,7 @@ class FeedView: UIView {
     lazy var tableView: UITableView = {
         let tv = UITableView()
         tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tv.register(FeedCell.self, forCellReuseIdentifier: FeedCell.identifier)
         return tv
     }()
     
@@ -78,6 +78,58 @@ class FeedView: UIView {
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
+
+class FeedCell: UITableViewCell {
+    static let identifier: String = "FeedCell"
+    
+    lazy var petImage: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(systemName: "person.crop.circle")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    lazy var breedLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = .preferredFont(forTextStyle: .subheadline)
+        lbl.text = "Breed"
+        return lbl
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy () {
+        addSubview(petImage)
+        addSubview(breedLabel)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            petImage.centerYAnchor.constraint(equalTo: centerYAnchor),
+            petImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            petImage.widthAnchor.constraint(equalToConstant: 40),
+            petImage.heightAnchor.constraint(equalTo: petImage.widthAnchor),
+            
+            breedLabel.leadingAnchor.constraint(equalTo: petImage.trailingAnchor, constant: 16),
+            breedLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
     }
 }
