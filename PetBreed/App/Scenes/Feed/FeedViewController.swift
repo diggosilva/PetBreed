4622170ba8af196bab4167b0e39b537b15e2b370
Diggo <rdiggosilva@gmail.com>
2024-10-10 20:07:57 -0300

    Implemented an alert to the add button to add the animal breed

--- PetBreed/App/Scenes/Feed/FeedViewController.swift
+++ PetBreed/App/Scenes/Feed/FeedViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class FeedViewController: UIViewController {
     
     let feedView = FeedView()
+    let viewModel = FeedViewModel()
     
     override func loadView() {
         super.loadView()
@@ -33,13 +34,39 @@ class FeedViewController: UIViewController {
     }
     
     @objc private func addButtonTapped() {
-        print("Toco no botão ADD")
+        let alert = UIAlertController(title: "Adicionar Raça", message: "Digite o nome da raça", preferredStyle: .alert)
+        alert.addTextField()
+        
+        let catAction = UIAlertAction(title: "Gato", style: .default, handler: { action in
+            if let text = alert.textFields?.first?.text, !text.isEmpty {
+                let catBreed = Animal(type: .cat, breed: text)
+                self.viewModel.listOfListOfAnimals.append(catBreed)
+                print("RaçaGatos: \(self.viewModel.listOfListOfAnimals)")
+                self.feedView.tableView.reloadData()
+            }
+        })
+        
+        let dogAction = UIAlertAction(title: "Cachorro", style: .default, handler: { action in
+            if let text = alert.textFields?.first?.text, !text.isEmpty {
+                let dogBreed = Animal(type: .dog, breed: text)
+                self.viewModel.listOfListOfAnimals.append(dogBreed)
+                print("RaçaCães: \(self.viewModel.listOfListOfAnimals)")
+                self.feedView.tableView.reloadData()
+            }
+        })
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(catAction)
+        alert.addAction(dogAction)
+        alert.addAction(cancel)
+        present(alert, animated: true)
     }
 }
 
 extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return viewModel.numbersOfRowsInSections(section: section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
